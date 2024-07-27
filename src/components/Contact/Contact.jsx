761d06785e9cea363599a9cd6c08fcd087b819c2
khojiakbarr <chinozbekov09@gmail.com>
2024-07-27 14:49:09 +0500

    upload

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,13 +7,13 @@ import "../Button/style.css";
 export default function Contact() {
   return (
     <div className="bg_contact">
-      <div className="flex container px-[15px] pt-[160px] pb-[177px] flex-wrap">
+      <div className="flex container px-[15px] pt-[200px] md:pt-[160px] pb-[177px] flex-wrap ">
         <div className="flex-1">
-          <h1 className="text-[#171b20] text-[30px] font-bold w-[400px]">
+          <h1 className="text-[#171b20] text-[25px] md:text-[30px] font-bold w-[400px]">
             Закажите замер, а также предварительную оценку объекта <br />
             <span className="text-[#ffc858]">БЕСПЛАТНО</span>
           </h1>
-          <h2 className="text-xl font-normal text-[#593f0d] mt-[49px] w-[312]">
+          <h2 className="text-xl font-normal text-[#593f0d] mt-[30px] md:mt-[49px] w-[312]">
             На бесплатном выезде инженер замерщик:
           </h2>
           <ul className="w-[300px] text-[#593f0d] text-xl mt-[13px] ">
@@ -28,16 +28,17 @@ export default function Contact() {
             </li>
           </ul>
         </div>
-        <div className="flex-1 flex flex-col items-center">
-          <form className="bg-[#FF8A00]  w-[400px] rounded-[10px] ">
+        {/* ///Right */}
+        <div className="flex-1 flex flex-col items-center mt-[30px] md:mt-0">
+          <form className="bg-[#FF8A00] w-[300px]  md:w-[400px] rounded-[10px] ">
             <div className="bg-white py-[28px] px-[30px] rounded-[10px] translate-x-[-10px] translate-y-[-15px]">
-              <h2 className="font-bold text-[#593f0d] md:text-3xl ">
+              <h2 className="font-bold text-[#593f0d] text-xl md:text-3xl ">
                 Укажите ваши контакты
               </h2>
-              <p className="text-[15px] font-normal mt-[7px]">
+              <p className="text-[13px] md:text-[15px] font-normal mt-[7px]">
                 и мы перезвоним вам, чтобы назначить удобное время для замера
               </p>
-              <div className="mt-[25px]">
+              <div className="mt-[15px] md:mt-[25px]">
                 <FloatingLabel
                   variant="standard"
                   label=" Ваше имя"
@@ -52,8 +53,8 @@ export default function Contact() {
                   type="tel"
                 />
               </div>
-              <div className="flex justify-center mt-[30px] mb-[30px]">
-                <button className="button px-[30px] py-[20px] rounded-[20px] shadow-2xl text-[13px] font-bold">
+              <div className="flex justify-center mt-[30px] mb-[15px] md:mb-[30px]">
+                <button className="button px-[15px] md:px-[30px] py-[15px] md:py-[20px] rounded-[20px] shadow-2xl text-[13px] font-bold">
                   ЗАКАЗАТЬ БЕСПЛАТНЫЙ ЗАМЕР
                 </button>
               </div>
