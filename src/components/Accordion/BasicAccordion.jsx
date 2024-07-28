fda1981eaf6456812b7a3d5dd673169f212fa6bd
khojiakbarr <chinozbekov09@gmail.com>
2024-07-28 15:53:58 +0500

    uplaod

--- src/components/Accordion/BasicAccordion.jsx
+++ src/components/Accordion/BasicAccordion.jsx
@@ -1,75 +1,68 @@
-import React from 'react'
-import { Accordion, AccordionContent, AccordionPanel, AccordionTitle } from "flowbite-react";
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionPanel,
+  AccordionTitle,
+} from "flowbite-react";
 
 export default function BasicAccordion() {
   return (
-    <Accordion className='border-x-0 rounded-none'>
-      <AccordionPanel className='bg-transparent'>
-        <AccordionTitle  className='bg-transparent hover:bg-transparent focus:border-none focus:outline-none focus:ring-0'>What is Flowbite?</AccordionTitle>
-        <AccordionContent className='transition-all duration-300'>
+    <Accordion className="border-x-0 rounded-none  ">
+      <AccordionPanel className="bg-transparent">
+        <AccordionTitle className="bg-transparent hover:bg-transparent focus:border-none focus:outline-none focus:ring-0">
+          What is Flowbite?
+        </AccordionTitle>
+        <AccordionContent className="transition-all duration-300">
           <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,
-            dropdowns, modals, navbars, and more.
+            Flowbite is an open-source library of interactive components built
+            on top of Tailwind CSS including buttons, dropdowns, modals,
+            navbars, and more.
           </p>
           <p className="text-gray-500 dark:text-gray-400">
-            Check out this guide to learn how to&nbsp;
-            <a
-              href="https://flowbite.com/docs/getting-started/introduction/"
-              className="text-cyan-600 hover:underline dark:text-cyan-500"
-            >
-              get started&nbsp;
-            </a>
-            and start developing websites even faster with components on top of Tailwind CSS.
+            Check out this guide to learn how to&nbsp; and start developing
+            websites even faster with components on top of Tailwind CSS.
           </p>
         </AccordionContent>
       </AccordionPanel>
       <AccordionPanel>
-        <AccordionTitle>Is there a Figma file available?</AccordionTitle>
+        <AccordionTitle className="bg-transparent hover:bg-transparent  focus:outline-none focus:ring-0">
+          Is there a Figma file available?
+        </AccordionTitle>
         <AccordionContent>
           <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
-            has a design equivalent in our Figma file.
+            Flowbite is first conceptualized and designed using the Figma
+            software so everything you see in the library has a design
+            equivalent in our Figma file.
           </p>
           <p className="text-gray-500 dark:text-gray-400">
-            Check out the
-            <a href="https://flowbite.com/figma/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-              Figma design system
-            </a>
-            based on the utility classes from Tailwind CSS and components from Flowbite.
+            Check out the based on the utility classes from Tailwind CSS and
+            components from Flowbite.
           </p>
         </AccordionContent>
       </AccordionPanel>
       <AccordionPanel>
-        <AccordionTitle>What are the differences between Flowbite and Tailwind UI?</AccordionTitle>
+        <AccordionTitle className="bg-transparent hover:bg-transparent  focus:outline-none focus:ring-0">
+          What are the differences between Flowbite and Tailwind UI?
+        </AccordionTitle>
         <AccordionContent>
           <p className="mb-2 text-gray-500 dark:text-gray-400">
-            The main difference is that the core components from Flowbite are open source under the MIT license, whereas
-            Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
-            components, whereas Tailwind UI offers sections of pages.
+            The main difference is that the core components from Flowbite are
+            open source under the MIT license, whereas Tailwind UI is a paid
+            product. Another difference is that Flowbite relies on smaller and
+            standalone components, whereas Tailwind UI offers sections of pages.
           </p>
           <p className="mb-2 text-gray-500 dark:text-gray-400">
-            However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
-            technical reason stopping you from using the best of two worlds.
+            However, we actually recommend using both Flowbite, Flowbite Pro,
+            and even Tailwind UI as there is no technical reason stopping you
+            from using the best of two worlds.
           </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-            <li>
-              <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                Flowbite Pro
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://tailwindui.com/"
-                rel="nofollow"
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Tailwind UI
-              </a>
-            </li>
-          </ul>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+            Learn more about these technologies:
+          </p>
+          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400"></ul>
         </AccordionContent>
       </AccordionPanel>
     </Accordion>
-  )
+  );
 }
---
+++ src/components/FiveShag/FiveShag.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import FiveShagCard from "./FiveShagCard";
+import tool from "../../assets/images/instrment_jp.png";
+import "../Button/style.css";
+
+export default function FiveShag() {
+  return (
+    <div className="container px-[15px]  flex-col items-center justify-center">
+      <div className="flex ">
+        <h2 className="text-[#593f0d] font-normal text-center text-[30px] md:text-[40px] mt-[40px] md:mt-[80px] mb-[18px] w-[600px]">
+          {" "}
+          <span className="text-[#ffc858] font-bold">5 шагов</span> до ремонта
+          вашей мечты
+        </h2>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex flex-col items-start gap-[25px] md:gap-[40px] ">
+          <FiveShagCard
+            num={"1"}
+            title={"Заявка"}
+            info={
+              "Вы оставляете заявку на сайте или по телефону. Мы договариваемся о встрече для точного замера и расчета стоимости"
+            }
+          />
+          <FiveShagCard
+            num={"2"}
+            title={"Замер"}
+            info={"Мы выезжаем на объект, делаем все необходимые замеры"}
+          />
+          <FiveShagCard
+            num={"3"}
+            title={"Смета, договор"}
+            info={"Состовляем точную смету, сроки, а также график платежей"}
+          />
+          <FiveShagCard
+            num={"4"}
+            title={"Передача ключей"}
+            info={
+              "После подписания договора вы передаете нам ключи, мы приступаем к работе"
+            }
+          />
+          <FiveShagCard
+            num={"5"}
+            title={"Ремонт"}
+            info={
+              "Делаем качественный ремонт, утверждая с вами каждый этап работ"
+            }
+          />
+          <FiveShagCard
+            num={"6"}
+            title={"Принимайте работу!"}
+            info={
+              "Принимайте работу! Получайте ваши ключи, а также уют в вашем доме"
+            }
+          />
+        </div>
+        <div className=" lg:w-[600px] object-cover hidden lg:block relative z-[-1]">
+          {" "}
+          <img className=" object-center scale-150" src={tool} alt="" />
+        </div>
+      </div>
+      <div className="flex justify-center my-[50px]">
+        <buttton className="button px-[20px] py-[15px] md:px-[30px] md:py-[20px] inline-block text-[13px] font-bold rounded-[15px] md:rounded-[20px] shadow-2xl cursor-pointer ">
+          ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ
+        </buttton>
+      </div>
+    </div>
+  );
+}
